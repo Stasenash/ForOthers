3ccd3bbad79b7023cc397095f1749245b4bb14ca
Stasenash <staslisenash@gmail.com>
2019-06-01 12:44:30 +0500

    added a limited size messages stack for Kati

---
+++ LimitedMessagesStack/LimitedMessagesStack/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace LimitedMessagesStack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbMessages = new System.Windows.Forms.TextBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tbMessages
+            // 
+            this.tbMessages.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbMessages.Location = new System.Drawing.Point(56, 36);
+            this.tbMessages.Multiline = true;
+            this.tbMessages.Name = "tbMessages";
+            this.tbMessages.ReadOnly = true;
+            this.tbMessages.Size = new System.Drawing.Size(616, 260);
+            this.tbMessages.TabIndex = 0;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 358);
+            this.Controls.Add(this.tbMessages);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbMessages;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ LimitedMessagesStack/LimitedMessagesStack/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LimitedMessagesStack
+{
+    public partial class Form1 : Form
+    {
+        private Random rnd = new Random();
+        private string[] str = new[] { "Разучивает с бродячим менестрелем песню",
+                                        "Так интенсивно бился в конвульсиях от прилетевшей молнии, что изрядно поколотил монстра",
+                                        "Наспор залез в берлогу к медведю",
+                                        "Косит дрынь-траву",
+                                        "Полетал на ковре",
+                                        "Проснулся возле затухающего костра",
+                                        "Разливает эль феям",
+                                        "Строит храм богу нашему Сёмге в стакане"};
+        private Queue<string> messages = new Queue<string>();
+        private Stack<string> reversMessages = new Stack<string>();
+        private int limit = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            tbMessages.Clear();
+            AddEvent();
+        }
+
+        private void AddEvent()
+        {
+            AddToQueue(GetEvent());
+            foreach (var m in messages)
+                reversMessages.Push(m);
+            foreach (var m in reversMessages)
+                tbMessages.Text += m + Environment.NewLine;
+            reversMessages.Clear();   
+        }
+
+        private string GetEvent()
+        {
+            var timeNow = DateTime.Now.ToShortTimeString();
+            return ">"+timeNow + " - " + str[rnd.Next(0, str.Length)];
+        }
+
+        private void AddToQueue(string message)
+        {
+            messages.Enqueue(message);
+            RemoveOferflow();
+        }
+
+        private void RemoveOferflow()
+        {
+            if (messages.Count > limit)
+                messages.Dequeue();
+        }
+        
+    }
+}
