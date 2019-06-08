88399c49e2d00c911160ac9887958f9e3508f795
Stasenash <staslisenash@gmail.com>
2019-06-08 22:19:22 +0500

    added a stack repeat for Jin

---
+++ StackRepeat/StackRepeat/FilesLoader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace StackRepeat
+{
+    public class FilesLoader
+    {
+        public static string[] LoadMessages() => File.ReadAllLines("Resources/Messages.txt");
+    }
+}
---
+++ StackRepeat/StackRepeat/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StackRepeat
+{
+    class Program
+    {
+        private static Stack<string> messages= new Stack<string>();
+        private int Limit = 10;
+
+        static void Main(string[] args)
+        {
+            ParseMsgs();
+            foreach (var m in messages)
+            {
+                Console.WriteLine(m);
+                Thread.Sleep(500);
+            }
+        }
+        static void ParseMsgs()
+        {
+            var msgs = FilesLoader.LoadMessages();
+            //for(......)
+            //messages.Push(msgs[i])
+            //removeoverflowing()
+        }
+
+        static void RemoveOverflowing()
+        {
+            //убрать переполнение стека
+        }
+
+    }
+}
