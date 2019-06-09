e02336e4f341e852b23631dd1d622891792c25bc
Stasenash <staslisenash@gmail.com>
2019-06-09 11:35:22 +0500

    LINQRepeat for Jin

---
+++ LINQRepeat/LINQRepeat/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQRepeat
+{
+    class Program
+    {
+        private static List<string> messages = FilesLoader.LoadMessages();
+
+        static void Main(string[] args)
+        {
+            foreach (var m in messages)
+                Console.WriteLine(m);
+        }
+
+        static void SortMessages()
+        {
+            //с помощью Линку отсортировать сообщения по алфавиту, но от Я до А
+            //оставить только первое
+            //проверить что выведется на экран
+        }
+    }
+}
