f30957c3274e30521cd45df3ac53829a032e6399
Stasenash <staslisenash@gmail.com>
2019-06-10 21:30:49 +0500

    Практика 5. Сортировка по длине строк в новый файл

---
+++ Практики на экзамен/Practic_5/Practic_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+//файл в папке bin\Debug\lines.txt
+/*Считайте из файла массив строк. Отсортируйте строки по длине по возрастанию и запишите в другой файл.*/
+
+namespace Practic_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("lines.txt");
+            lines = lines.OrderBy(e => e.Length).ToArray();
+            File.WriteAllLines( "finallines.txt",lines);
+        }
+    }
+}
