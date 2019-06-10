451da919bfd15d95808fb4f0a437445113b42b9c
Stasenash <staslisenash@gmail.com>
2019-06-10 23:19:24 +0500

    Практика 10. Словарь по первой букве слова

---
+++ Практики на экзамен/Practic_10/Practic_10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+//файл находится в bin\Debug\words.txt
+
+/*Считайте из файла массив слов. Создайте словарь списков слов, где ключом будет первая буква каждого слова.*/
+namespace Practic_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = File.ReadAllText("words.txt").ToLower().Split(' ');
+            var dict = new Dictionary<char, List<string>>();
+            foreach (var word in words)
+                if (!dict.ContainsKey(word[0]))
+                    dict.Add(word[0], new List<string>() { word });
+                else
+                    dict[word[0]].Add(word);
+        }
+    }
+}
