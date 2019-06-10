033ac9f006bab66fe50cb37b801f17d126293fdc
Stasenash <staslisenash@gmail.com>
2019-06-10 21:58:03 +0500

    Практика 7. Простые числа из массива чисел

---
+++ Практики на экзамен/Practic_7/Practic_7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+//файл лежит в bin\Debug\array.txt
+
+/*Считайте из файла массив чисел. Выведите на экран все простые числа из файла.*/
+
+namespace Practic_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = File.ReadAllText("array.txt").Split(' ');
+            List<int> nums = new List<int>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                bool flag = true;
+                for (int j = 2; j < int.Parse(array[i]); j++)
+                {
+                    if (int.Parse(array[i]) % j == 0)
+                        flag = false;
+                }
+                if (flag)
+                    nums.Add(int.Parse(array[i]));
+            }
+            foreach (var num in nums)
+                Console.WriteLine(num);
+        }
+    }
+}
