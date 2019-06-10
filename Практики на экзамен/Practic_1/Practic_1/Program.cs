576c033f065a386ba222481718c7e93c8137f3f1
Stasenash <staslisenash@gmail.com>
2019-06-10 19:57:55 +0500

    Практика 1. Разбить предложения на слова и тд. (в кач-ве текста отрывок из ведьмака)

---
+++ Практики на экзамен/Practic_1/Practic_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO; //подключили библиотеку работы с файлами
+
+//файл с текстом находится в папке bin\Debug
+
+/*
+ Считайте из файла текст. Разбейте его по предложениям. Каждое предложение
+ разбейте на слова. Подсчитайте кол-во всех предложений, всех слов и 
+ выведите на экран список уникальных слов в тексте
+  */
+
+namespace Practic_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentences = File.ReadAllText("array.txt").Split('.');
+            for (int i = 0; i < sentences.Length; i++)
+                sentences[i] = sentences[i].Trim();
+            sentences = sentences.Where(x => x!=string.Empty).ToArray();
+            var words = File.ReadAllText("array.txt").Split(' ');
+            for (int i = 0; i < words.Length; i++)
+                words[i] = words[i].Replace(",","").Replace("—", "").Replace(".","").Replace("?","").Replace("!","").ToLower().Trim();
+            Console.WriteLine($"Кол-во предложений: {sentences.Length}");
+            Console.WriteLine($"Кол-во слов: {words.Length}");
+            words = words.Where(x => x != string.Empty).Distinct().ToArray();
+            Console.WriteLine($"Кол-во уникальных слов: {words.Length}");
+            Console.WriteLine("Список уникальных слов:");
+            foreach (var word in words)
+                Console.WriteLine(word);
+        }
+    }
+}
