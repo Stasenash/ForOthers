7ac87e443f49d158a260aecef84d984309e78f4a
Stasenash <staslisenash@gmail.com>
2019-06-10 23:34:40 +0500

    Практика 11. Самая частовстречаемая буква

---
+++ Практики на экзамен/Practic_11/Practic_11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+//файл в папке bin\Debug\text.txt
+/* Считайте из файла текст. Найдите самую часто встречающуюся букву в этом тексте. */
+namespace Practic_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllText("text.txt").ToLower().Replace(" ","");
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            foreach(var e in text)
+            {
+                if (!dict.ContainsKey(e))
+                    dict.Add(e, 1);
+                else
+                    dict[e]++;
+            }
+            var max = dict.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+            Console.WriteLine(max);
+        }
+    }
+}
