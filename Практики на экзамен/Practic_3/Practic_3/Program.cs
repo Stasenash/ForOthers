4f45d048c0a632cca45681298a4a81a24ce7c1be
Stasenash <staslisenash@gmail.com>
2019-06-10 20:33:13 +0500

    Практика 3. Самая длинная строка в обр. порядке

---
+++ Практики на экзамен/Practic_3/Practic_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+//Файл в папке bin\Debug\lines.txt
+
+/* Считайте из файла массив строк. Найдите самую длинную строку и выведите на экран ее в обратном порядке.*/
+
+namespace Practic_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("lines.txt");
+            var line = lines.OrderByDescending(e=>e.Length).First();
+            var newline = "";
+            for (int i = line.Length - 1; i >= 0; i--)
+                newline += line[i];
+            Console.WriteLine(newline);
+        }
+    }
+}
