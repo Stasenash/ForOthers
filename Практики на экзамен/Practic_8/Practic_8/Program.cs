a7e2a6813caa4e4b6bf8b20098f5f35574b3c844
Stasenash <staslisenash@gmail.com>
2019-06-10 22:50:01 +0500

    Практика 8. Шифр цезаря

---
+++ Практики на экзамен/Practic_8/Practic_8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+//файл лежит в bin\Debug\cesar.txt
+
+/* Считайте из файла текст и число. Зашифруйте считанный текст по шифру цезаря, использую считанное число, как основание шифра. 
+ * Если основание шифра равно 2, то буква А -> В, Б -> Г и т.д.
+ п.с. она у меня пока что не идет по кругу, то есть после Я идет всякая хрень(*/
+
+namespace Practic_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cesar = File.ReadAllLines("cesar.txt");
+            var lines = new List<string>();
+            int step = int.Parse(cesar[cesar.Length - 1]);
+            for (int i = 0; i < cesar.Length - 1; i++)
+            {
+                var line = cesar[i];
+                var newline = "";
+                for (int j = 0; j < line.Length; j++)
+                    newline += (char)(line[j] + step);
+                lines.Add(newline);
+            }
+            lines.Add(step.ToString());
+            File.WriteAllLines("cesar.txt", lines);
+        }
+    }
+}
